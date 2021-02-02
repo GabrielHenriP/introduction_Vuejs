b9d7fad3edc0246de36602d8318a74cab761e989
gabriel <soadhenri@gmail.com>
2021-02-02 20:56:35 -0300

    first commit of 'vue-crash-todolist'

---
+++ vue-crash-todolist/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <Header />
+    <AddTodo />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Header from './components/layouts/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Todos,
+    AddTodo
+  },
+  data(){
+    return {
+      todos:[
+        {
+          id: 1,
+          title: 'todo 1',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'todo 2',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'todo 3',
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter( todo => todo.id !== id)
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn:hover{
+    background: #bbb;
+    cursor: pointer;
+  }
+</style>
---
+++ vue-crash-todolist/src/components/TodoItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete': newTodo.completed}" >
+    <p>
+      <input type="checkbox" v-on:change="markComplete"> 
+      {{todo.title}} 
+      <button @click="$emit('del-todo', todo.id)" class="del" >X</button>
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"TodoItem",
+  props: ["todo"],
+  data(){
+    return {
+      newTodo: this.todo
+    }
+  },
+  methods: {
+    markComplete(){
+      console.log(this.newTodo.completed)
+      this.newTodo.completed = !this.newTodo.completed
+    }
+  }
+}
+</script>
+
+<style scope>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del{
+    background: #ff0000;
+    color: #fff;
+    border:none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
