e58ef75284b94ab2f8bfe8c9720e7d5f896ad2a4
gabriel <soadhenri@gmail.com>
2021-02-03 08:39:05 -0300

    add delete methods with axios

--- vue-crash-todolist/src/App.vue
+++ vue-crash-todolist/src/App.vue
@@ -27,7 +27,11 @@ export default {
   },
   methods: {
     deleteTodo(id){
-      this.todos = this.todos.filter( todo => todo.id !== id)
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then( () => this.todos = this.todos.filter( todo => todo.id !== id))
+        .catch( e => console.log(e))
+
+      
     },
     addTodo(newTodo) {
       const { title, completed } = newTodo;
